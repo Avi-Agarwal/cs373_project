b5ec1ea4b7060e5c2fec143c3da46ccf4e3d1599
Andrew Furman <andrew_furman@hermanmiller.com>
2019-11-16 17:29:04 -0500

    initial data loading

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
+from pandas import DataFrame
+import os
+
 if __name__ == '__main__':
     print('Hey Team')
+
+
+def loadData():
+    data = DataFrame({'text': [], 'classifier': []})
+
+    spamFiles = ['enron1/spam/' + f for f in os.listdir('enron1/spam')]
+    hamFiles = ['enron1/ham/' + f for f in os.listdir('enron1/ham')]
+
+    spam = []
+    spam_names = []
+    for file in spamFiles:
+        with open(file, 'r', encoding="utf8", errors='ignore') as f:
+            text = f.read()
+            spam.append({'text': text, 'classifier': 'spam'})
+            spam_names.append(file)
+
+    data = data.append(DataFrame(spam, spam_names))
+
+    ham = []
+    ham_names = []
+    for file in hamFiles:
+        with open(file, 'r', encoding="utf8", errors='ignore') as f:
+            text = f.read()
+            ham.append({'text': text, 'classifier': 'not spam'})
+            ham_names.append(file)
+
+    data = data.append(DataFrame(ham, ham))
+
+    print(data.describe())
+
+
+loadData()
